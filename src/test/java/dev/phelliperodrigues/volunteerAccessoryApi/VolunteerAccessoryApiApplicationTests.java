e8c43d362f1c4230197706852c7178fba6baca7a
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-09-05 22:43:15 -0300

    chore(:rotating_light:): create test to main

--- src/test/java/dev/phelliperodrigues/volunteerAccessoryApi/VolunteerAccessoryApiApplicationTests.java
+++ src/test/java/dev/phelliperodrigues/volunteerAccessoryApi/VolunteerAccessoryApiApplicationTests.java
@@ -1,13 +1,24 @@
 package dev.phelliperodrigues.volunteerAccessoryApi;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Profile("test")
 class VolunteerAccessoryApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testApplication() {
+		MockedStatic<SpringApplication> utilities = Mockito.mockStatic(SpringApplication.class);
+		utilities.when((MockedStatic.Verification) SpringApplication.run(VolunteerAccessoryApiApplication.class, new String[]{})).thenReturn(null);
+		VolunteerAccessoryApiApplication.main(new String[]{});
+		assertThat(SpringApplication.run(VolunteerAccessoryApiApplication.class)).isEqualTo(null);
 	}
 
 }
