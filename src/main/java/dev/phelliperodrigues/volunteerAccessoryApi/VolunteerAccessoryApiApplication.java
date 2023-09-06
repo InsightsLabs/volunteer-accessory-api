9ee694bb6f7541803d3c2acecdc8e072811afa73
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-09-05 21:07:49 -0300

    chore(:tada:): first commit

---
+++ src/main/java/dev/phelliperodrigues/volunteerAccessoryApi/VolunteerAccessoryApiApplication.java
@@ -0,0 +1,13 @@
+package dev.phelliperodrigues.volunteerAccessoryApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerAccessoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerAccessoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/phelliperodrigues/volunteerAccessoryApi/VolunteerAccessoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phelliperodrigues.volunteerAccessoryApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerAccessoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
