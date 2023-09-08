0e8d16653b1ef7b7b781648f50ea13d397c51ee8
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-09-08 20:11:32 -0300

    chore(:rotating_light:): adjusted mvn script to verify80 80 80

--- src/main/java/dev/phelliperodrigues/volunteerAccessoryApi/VolunteerAccessoryApiApplication.java
+++ src/main/java/dev/phelliperodrigues/volunteerAccessoryApi/VolunteerAccessoryApiApplication.java
@@ -3,11 +3,25 @@ package dev.phelliperodrigues.volunteerAccessoryApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.plaf.PanelUI;
+
 @SpringBootApplication
 public class VolunteerAccessoryApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VolunteerAccessoryApiApplication.class, args);
+	}
+
+
+	public void testApplication(Integer number) {
+
+		if (number <0) {
+			System.out.println("Number is negative");
+		} else if (number > 0) {
+			System.out.println("Number is positive");
+		} else {
+			System.out.println("Number is zero");
+		}
 	}
 
 }
