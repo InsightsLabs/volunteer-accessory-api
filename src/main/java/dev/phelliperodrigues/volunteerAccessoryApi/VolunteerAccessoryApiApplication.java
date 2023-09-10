5ebb5e9df59c06cc5de1576b04f0db670e48ba94
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-09-10 13:57:41 -0300

    SVC-52 Configurando Quartz

--- src/main/java/dev/phelliperodrigues/volunteerAccessoryApi/VolunteerAccessoryApiApplication.java
+++ src/main/java/dev/phelliperodrigues/volunteerAccessoryApi/VolunteerAccessoryApiApplication.java
@@ -1,13 +1,16 @@
 package dev.phelliperodrigues.volunteerAccessoryApi;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VolunteerAccessoryApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VolunteerAccessoryApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(VolunteerAccessoryApiApplication.class).bannerMode(Banner.Mode.OFF).run(args);
+    }
 
 }
--- src/test/java/dev/phelliperodrigues/volunteerAccessoryApi/VolunteerAccessoryApiApplicationTests.java
+++ src/test/java/dev/phelliperodrigues/volunteerAccessoryApi/VolunteerAccessoryApiApplicationTests.java
@@ -1,8 +1,10 @@
 package dev.phelliperodrigues.volunteerAccessoryApi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
@@ -10,17 +12,22 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 class VolunteerAccessoryApiApplicationTests {
 
 	@Test
 	void testApplication() {
-		/*MockedStatic<SpringApplication> utilities = Mockito.mockStatic(SpringApplication.class);
+		MockedStatic<SpringApplication> utilities = Mockito.mockStatic(SpringApplication.class);
 		utilities.when((MockedStatic.Verification) SpringApplication.run(VolunteerAccessoryApiApplication.class, new String[]{})).thenReturn(null);
 		VolunteerAccessoryApiApplication.main(new String[]{});
 		assertThat(SpringApplication.run(VolunteerAccessoryApiApplication.class)).isEqualTo(null);
-	*/
+	}
+
+	@Test
+	void validCreationClass() {
+		VolunteerAccessoryApiApplication volunteerAccessoryApiApplication = new VolunteerAccessoryApiApplication();
+		assertThat(volunteerAccessoryApiApplication).isNotNull();
 	}
 
 }
