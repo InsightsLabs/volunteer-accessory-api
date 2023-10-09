ba3e5ea1667ba951f7be8767d4a5913be536a945
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-10-09 16:02:28 -0300

    SVC-17 Inplement ResponseStatusExceptionControllerAdvice

---
+++ src/main/java/dev/phelliperodrigues/volunteerAccessoryApi/application/web/rest/handlers/ResponseStatusExceptionControllerAdvice.java
@@ -0,0 +1,29 @@
+package dev.phelliperodrigues.volunteerAccessoryApi.application.web.rest.handlers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Date;
+
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class ResponseStatusExceptionControllerAdvice {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ApiError> handleResponseStatusException(ResponseStatusException ex) {
+
+        return ResponseEntity.status(ex.getStatusCode()).body(new ApiError(
+                new Date(),
+                ex.getBody().getTitle(),
+                ex.getBody().getStatus(),
+                ex.getBody().getDetail(),
+                ex.getMessage()
+        ));
+    }
+}
